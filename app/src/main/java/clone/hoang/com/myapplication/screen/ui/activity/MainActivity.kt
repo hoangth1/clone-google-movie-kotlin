b32c4cf829cb7ec8740ec045a2236a90ea6570be
thang.huy.hoang <FRAMGIA\thang.huy.hoang@framgia0384-pc.framgia.com>
2018-12-21 17:30:10 +0700

    create main activity and main fragmet

--- app/src/main/java/clone/hoang/com/myapplication/screen/ui/activity/MainActivity.kt
+++ app/src/main/java/clone/hoang/com/myapplication/screen/ui/activity/MainActivity.kt
@@ -1,19 +1,28 @@
 package clone.hoang.com.myapplication.screen.ui.activity
 
 import android.os.Bundle
+import android.support.v4.content.res.ResourcesCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import clone.hoang.com.myapplication.R
 import clone.hoang.com.myapplication.screen.ui.base.BaseActivity
 import clone.hoang.com.myapplication.screen.ui.fragment.main.MainFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
         if (savedInstanceState == null)
             supportFragmentManager.beginTransaction().apply {
-                add(R.id.root_contaier, MainFragment.newInstance(), "")
+                add(R.id.container, MainFragment.newInstance(), "")
                 commit()
             }
+        ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.app_name, R.string.app_name).apply {
+            drawerArrowDrawable.color = ResourcesCompat.getColor(resources, R.color.mercury, null)
+            drawer_layout.addDrawerListener(this)
+            syncState()
+        }
     }
 }
--- app/src/main/java/clone/hoang/com/myapplication/screen/ui/fragment/main/MainFragment.kt
+++ app/src/main/java/clone/hoang/com/myapplication/screen/ui/fragment/main/MainFragment.kt
@@ -1,7 +1,13 @@
 package clone.hoang.com.myapplication.screen.ui.fragment.main
 
+import android.support.v4.content.ContextCompat
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.TextUtils
+import android.text.style.ForegroundColorSpan
 import clone.hoang.com.myapplication.R
 import clone.hoang.com.myapplication.databinding.FragmentMainBinding
+import clone.hoang.com.myapplication.screen.ui.activity.MainActivity
 import clone.hoang.com.myapplication.screen.ui.base.BaseFragment
 import com.android.databinding.library.baseAdapters.BR
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -15,6 +21,26 @@ class MainFragment : BaseFragment<FragmentMainBinding, MainViewModel>() {
     override val viewModel: MainViewModel by viewModel()
     override val layoutRes: Int = R.layout.fragment_main
     override fun initComps() {
-    }
+        setHasOptionsMenu(true)
+        (activity as MainActivity).apply {
+            val firstPart = SpannableString(getString(R.string.google_play))
+            val lastPart = SpannableString(getString(R.string.movie_and_tv))
 
+            firstPart.setSpan(
+                ForegroundColorSpan(ContextCompat.getColor(applicationContext, R.color.mercury)),
+                0,
+                firstPart.length,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+
+            lastPart.setSpan(
+                ForegroundColorSpan(ContextCompat.getColor(applicationContext, R.color.colorAccent)),
+                0,
+                lastPart.length,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            supportActionBar?.title = TextUtils.concat(firstPart, " ", lastPart)
+
+        }
+    }
 }
